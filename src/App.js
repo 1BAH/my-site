208f356e2e1be0f12e22cac6b8ce3b5d89aa41a5
Iван <kalinin.is@phystech.edu>
2023-03-01 17:56:52 +0300

    TODO

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+import NavBar from './NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <NavBar />
+
+            <div style={{ textAlign: 'center' }}>
+                <Spinner
+                    animation="border"
+                    style={{
+                        color: '#1A237E',
+                        height: '25rem',
+                        width: '25rem',
+                        margin: '3rem',
+                    }}
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navbar, Container, Nav, Button, Form } from 'react-bootstrap';
+
+const NavBar = () => (
+    <Navbar expand="lg" style={{ backgroundColor: '#1A237E' }}>
+        <Container fluid>
+            <Navbar.Brand href="#" style={{ color: 'orange' }}>
+                TP info & samples
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+                    <Nav.Link className="navLink" href="#home" disabled>
+                        Home
+                    </Nav.Link>
+                </Nav>
+                <Form className="d-flex">
+                    <Form.Control type="search" placeholder="// TODO :)" className="me-2" aria-label="Search" style={{ color: 'indigo' }} disabled />
+                    <Button variant="outline-secondary" disabled>
+                        Search
+                    </Button>
+                </Form>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+);
+
+export default NavBar;
