20880bb3ca6637be63f71361839501750b963eb7
Iван <kalinin.is@phystech.edu>
2023-03-03 00:33:31 +0300

    pages updated

--- src/elements/NavBar.js
+++ src/elements/NavBar.js
@@ -4,16 +4,16 @@ import { Navbar, Container, Nav, Button, Form } from 'react-bootstrap';
 const NavBar = () => (
     <Navbar expand="lg" style={{ backgroundColor: '#1A237E' }}>
         <Container fluid>
-            <Navbar.Brand href="/" style={{ color: 'orange' }}>
+            <Navbar.Brand href="/tp-info/#" style={{ color: 'orange' }}>
                 TP info & samples
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
                 <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-                    <Nav.Link className="navLink" href="/tp-info/home">
+                    <Nav.Link className="navLink" href="/tp-info/#/home">
                         Home
                     </Nav.Link>
-                    <Nav.Link className="navLink" href="/tp-info/gdb">
+                    <Nav.Link className="navLink" href="/tp-info/#/gdb">
                         gdb
                     </Nav.Link>
                 </Nav>
