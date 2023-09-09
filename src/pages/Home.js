a965525ede616e15dfaa3e01d941bd0efa0ec834
Iван <kalinin.is@phystech.edu>
2023-09-10 02:06:17 +0300

    fix position

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 import styles from '../css/Text.module.css';
 import manuscript from './manuscript.png';
 import basicStyles from '../css/Default.module.css';
@@ -7,10 +6,9 @@ import basicStyles from '../css/Default.module.css';
 const Home = () => (
     <>
         <h1 className={styles.header}>Сборник конспектов и прочего</h1>
-        <div style={{ textAlign: 'center' }}>
+        <div style={{ textAlign: 'center', width: '90%' }}>
             <img src={manuscript} alt="manuscript" className={basicStyles.picture} />
         </div>
-        <Outlet />
     </>
 );
 
