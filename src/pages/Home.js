27fd1feb51efceca087c216ea4bce0f2dc2ac588
Iван <kalinin.is@phystech.edu>
2023-09-10 22:59:13 +0300

    fix position

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Row } from 'react-bootstrap';
 import styles from '../css/Text.module.css';
 import manuscript from './manuscript.png';
 import basicStyles from '../css/Default.module.css';
@@ -10,9 +11,11 @@ const Home = () => {
     return (
         <>
             <h1 className={styles.header}>Сборник конспектов и прочего</h1>
-            <div style={{ textAlign: 'center', width: '90%' }}>
-                <img src={manuscript} alt="manuscript" className={basicStyles.picture} />
-            </div>
+            <Row className="justify-content-center" style={{ width: '100%' }}>
+                <div style={{ textAlign: 'center', width: '90%' }}>
+                    <img src={manuscript} alt="manuscript" className={basicStyles.picture} />
+                </div>
+            </Row>
         </>
     );
 };
