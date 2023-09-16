86321cdce6936ae7c6a00aeb17f5e97dacb5d919
Iван <kalinin.is@phystech.edu>
2023-09-16 23:47:32 +0300

    smartphone optimisation

--- src/cards/Cards.js
+++ src/cards/Cards.js
@@ -3,7 +3,7 @@ import { Container, Row } from 'react-bootstrap';
 
 const Cards = ({ cards }) => (
     <Container>
-        <Row xs={3} md={5} className="g-4 m-0 justify-content-md-center">
+        <Row xs={2} md={5} className="g-4 m-0 justify-content-md-center">
             {cards}
         </Row>
     </Container>
