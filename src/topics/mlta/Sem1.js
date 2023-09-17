99da95c6f178e19357a4787ff92c298365856181
Iван <kalinin.is@phystech.edu>
2023-09-17 21:02:38 +0300

    add titles

--- src/topics/mlta/Sem1.js
+++ src/topics/mlta/Sem1.js
@@ -5,26 +5,30 @@ import Separator from '../../elements/Separator';
 import Absent from './Absent';
 import fl from '../../cards/img/lang.png';
 import Img from '../../utils/Image';
+import title from '../../hooks/title';
 
-const Sem1 = () => (
-    <>
-        <h3 className={styles.header}>Первый допсеминар (осень)</h3>
+const Sem1 = () => {
+    title('JK [МЛТА] Формалки');
 
-        <Img img={fl} width={20} />
+    return (
+        <>
+            <h3 className={styles.header}>Первый допсеминар (осень)</h3>
 
-        <Row className="justify-content-center" style={{ width: '100%' }}>
-            <div className={styles.block} style={{ textAlign: 'center', width: '80%' }}>
-                Тема: <div className={styles.highlight}>Основы формальных языков</div>
-                <br />
-                <div className={styles.quote}>
-                    Важное замечание! Я перепутал алфавиты и слова, поэтому сказал, что слова могут быть счетными. На самом деле это всегда
-                    ОБЯЗАТЕЛЬНО конечные последовательности.
-                </div>
-                <Separator />
-                <br />
-                <p>Видео</p>
-                <Absent />
-                {/* <iframe
+            <Img img={fl} width={20} />
+
+            <Row className="justify-content-center" style={{ width: '100%' }}>
+                <div className={styles.block} style={{ textAlign: 'center', width: '80%' }}>
+                    Тема: <div className={styles.highlight}>Основы формальных языков</div>
+                    <br />
+                    <div className={styles.quote}>
+                        Важное замечание! Я перепутал алфавиты и слова, поэтому сказал, что слова могут быть счетными. На самом деле это всегда
+                        ОБЯЗАТЕЛЬНО конечные последовательности.
+                    </div>
+                    <Separator />
+                    <br />
+                    <p>Видео</p>
+                    <Absent />
+                    {/* <iframe
                         width="80%"
                         src="..."
                         title="Lambdas"
@@ -33,33 +37,34 @@ const Sem1 = () => (
                         allowFullScreen
                         style={{ aspectRatio: '16 / 9' }}
                     /> */}
-                <br />
-                <br />
-                <Separator />
-                <br />
-                <p>Записи</p>
-                <iframe
-                    title="turing"
-                    src="https://drive.google.com/file/d/1ss1lZ5aI6lp-WELMaFKEq2qeDPnWtgrR/preview"
-                    width="60%"
-                    allow="autoplay"
-                    style={{ aspectRatio: '11 / 10' }}
-                />
-                <br />
-                <br />
-                <Separator />
-                <br />
-                <p>Решения прошлых лет</p>
-                <iframe
-                    title="1-extra"
-                    src="https://drive.google.com/file/d/1shHhZGWiPTyrDe0MRD841uYElN6jSfWF/preview"
-                    width="60%"
-                    allow="autoplay"
-                    style={{ aspectRatio: '11 / 10' }}
-                />
-            </div>
-        </Row>
-    </>
-);
+                    <br />
+                    <br />
+                    <Separator />
+                    <br />
+                    <p>Записи</p>
+                    <iframe
+                        title="turing"
+                        src="https://drive.google.com/file/d/1ss1lZ5aI6lp-WELMaFKEq2qeDPnWtgrR/preview"
+                        width="60%"
+                        allow="autoplay"
+                        style={{ aspectRatio: '11 / 10' }}
+                    />
+                    <br />
+                    <br />
+                    <Separator />
+                    <br />
+                    <p>Решения прошлых лет</p>
+                    <iframe
+                        title="1-extra"
+                        src="https://drive.google.com/file/d/1shHhZGWiPTyrDe0MRD841uYElN6jSfWF/preview"
+                        width="60%"
+                        allow="autoplay"
+                        style={{ aspectRatio: '11 / 10' }}
+                    />
+                </div>
+            </Row>
+        </>
+    );
+};
 
 export default Sem1;
