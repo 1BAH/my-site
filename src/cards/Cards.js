993554beb3f2370fa6453347c4fee3d121f284af
Iван <kalinin.is@phystech.edu>
2023-10-23 21:51:46 +0300

    v2.4.1 - increased cards count for xl devices

--- src/cards/Cards.js
+++ src/cards/Cards.js
@@ -3,7 +3,7 @@ import { Container, Row } from 'react-bootstrap';
 
 const Cards = ({ cards }) => (
     <Container fluid>
-        <Row xs={1} sm={2} lg={4} md={3} className=" justify-content-md-center">
+        <Row xs={1} sm={2} lg={4} md={3} xl={5} className=" justify-content-md-center">
             {cards}
         </Row>
     </Container>
