5f604994932b6ad303f66a049c6d5dd6691321e6
Iван <kalinin.is@phystech.edu>
2024-02-21 00:36:46 +0300

    v2.9.1 - add missing file

---
+++ src/topics/mlta/spring/Sem9.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import img from '../../../cards/img/le.png';
+import TemplateSem from '../TemplateSem';
+
+const Sem9 = () => (
+    <TemplateSem
+        title="Девятый допсеминар (весна)"
+        topic="Порядки"
+        img={img}
+        description="Девятый допсеминар по матлогике. Тема: порядки."
+        videoUrl="https://www.youtube.com/embed/7m8Yqhqjotc?si=P9JbXYxABYqX4LO8"
+        notesUrl="https://drive.google.com/file/d/14pa8EMlY5-hZe8v4_ZgocpIlrRiXRlyO/preview"
+        solutionsUrl="https://drive.google.com/file/d/1P3bPny82N76PtTTsC2zISIGaEfZETCZ9/preview"
+    />
+);
+
+export default Sem9;
