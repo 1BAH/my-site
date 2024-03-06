42e469fb3b738a69e15da75e59aa1e4e78ab728a
Iван <kalinin.is@phystech.edu>
2024-03-06 15:56:07 +0300

    v2.10.4 - mistake

--- src/topics/mlta/spring/Sem10.js
+++ src/topics/mlta/spring/Sem10.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import img from '../../../cards/img/omega.png';
 import TemplateSem from '../TemplateSem';
+import styles from '../../../css/Text.module.css';
 
 const Sem10 = () => (
     <TemplateSem
         title="Десятый допсеминар (весна)"
         topic="Ординалы"
         img={img}
+        comment={
+            <div className={styles.quote}>
+                Важное замечание! <b>В 7 номере есть ошибка</b>:
+                <br />
+                Пункт г неверный - начальный отрезок может быть несобственным.
+            </div>
+        }
         description="Десятый допсеминар по матлогике. Тема: ординалы."
         videoUrl="https://www.youtube.com/embed/xY3TRO49SRA?si=Iv8i60SqTUtXemBr"
         notesUrl="https://drive.google.com/file/d/16OXAP4GDxih9aga3EjKkrFJjqOZly3r4/preview"
